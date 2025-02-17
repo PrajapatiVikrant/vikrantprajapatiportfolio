d7c1be49dbdb369624cc216ed93003738b7699c5
PrajapatiVikrant <prajapati865039@gmail.com>
2025-02-17 05:16:12 -0800

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './Component/Navbar'
-import Home from './pages/Home'
-import About from './pages/About'
-import Experience from './pages/Experience'
-import Projects from './pages/Projects'
-import Footer from './component/Footer'
+import Navbar from './Navbar'
+import Home from './Home'
+import About from './About'
+import Experience from './Experience'
+import Projects from './Projects'
+import Footer from './Footer'
 
 function App() {
   
